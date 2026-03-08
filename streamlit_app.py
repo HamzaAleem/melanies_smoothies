133f93e3397a30e41485e977fce3024eceddbbed
Hamza Aleem <41679853+HamzaAleem@users.noreply.github.com>
2026-03-08 17:30:11 +0500

    Update Snowflake connection method in streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("Name on Smoothie will be:",name_on_order )
 
 
-cnx = st.connection('snowflake')
+cnx = st.secrets["snowflake"]
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)

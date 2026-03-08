d84601c77166933cd1d0dcb51a054822d4cf4237
Hamza Aleem <41679853+HamzaAleem@users.noreply.github.com>
2026-03-08 22:04:37 +0500

    Fix connection to Snowflake account in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("Name on Smoothie will be:",name_on_order )
 
 st.write(st.secrets)
-cnx = st.secrets["snowflake"]
+cnx = st.secrets["snowflake"]["account"]
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)

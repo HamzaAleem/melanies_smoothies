c03b701f846e80b8f8b1240b1a79bdbf3747d270
Hamza Aleem <41679853+HamzaAleem@users.noreply.github.com>
2026-03-08 22:32:11 +0500

    Import Session from snowflake.snowpark

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import snowflake.connector
+from snowflake.snowpark import Session 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: {st.__version__}")
 st.write(

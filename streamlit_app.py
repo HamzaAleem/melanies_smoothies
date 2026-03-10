a340733dcc7a60cc45cd617b51074e2619c3b4be
Hamza Aleem <41679853+HamzaAleem@users.noreply.github.com>
2026-03-11 01:52:23 +0500

    Enhance dataframe display and execution flow
    Added functionality to display dataframes and stop execution at certain points.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,14 @@ st.write("Name on Smoothie will be:",name_on_order )
 cnx = st.secrets["snowflake"]["account"]
 session = Session.builder.configs(st.secrets["snowflake"]).create()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:',my_dataframe,max_selections=5
 )
@@ -29,6 +36,9 @@ if ingredients_list:
 
     for fruit_choosen in ingredients_list:
       ingredients_string+=fruit_choosen
+
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       st.subheader(fruit_chosen + 'Nutrition Information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)  
       sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
